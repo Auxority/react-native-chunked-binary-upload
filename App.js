2e9da91dcf660d5fd22caef2fdf0b46e298408a0
Auxority <gide0013@hz.nl>
2023-12-19 19:18:04 +0100

    non working attempt

--- App.js
+++ App.js
@@ -1,30 +1,12 @@
-// import the Upload.js component and create a simple UI to render it:
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
-import NativeUploady from "@rpldy/native-uploady";
 import Upload from "./Upload";
-import { composeEnhancers } from "@rpldy/uploader";
-import ChunkedSender from "@rpldy/chunked-sender";
-
-const customChunkedSender = ChunkedSender({
-  chunked: true,
-  chunkSize: 2 * 1024 * 1024, // 2MB
-  retries: 0, // TODO: change to 3
-  parallel: 0,
-});
-
-const enhancers = composeEnhancers(customChunkedSender);
 
 const App = () => (
-  <NativeUploady
-    enhancers={enhancers}
-    destination={{ url: "https://starfilesupload.requestcatcher.com/test" }}
-  >
-    <View style={styles.container}>
-      <Text>Upload files</Text>
-      <Upload />
-    </View>
-  </NativeUploady>
+  <View style={styles.container}>
+    <Text>Upload files</Text>
+    <Upload />
+  </View>
 );
 
 const styles = StyleSheet.create({
--- Upload.js
+++ Upload.js
@@ -1,62 +1,111 @@
-import React, { useCallback, useContext } from "react";
+import React, { useCallback } from "react";
 import { View, Button } from "react-native";
 import * as DocumentPicker from "expo-document-picker";
-import { UploadyContext, useItemStartListener, useItemProgressListener, useItemErrorListener, useItemFinishListener } from "@rpldy/native-uploady";
+import sha256 from "./sha256";
+import * as Crypto from "expo-crypto";
 
 const Upload = () => {
-  const uploadyContext = useContext(UploadyContext);
+    const UPLOAD_ENDPOINT = "https://upload.starfiles.co/chunk";
+    // const UPLOAD_ENDPOINT = "https://starfilesupload.requestcatcher.com/test"
 
-  useItemStartListener((item) => {
-    console.log(`Starting to upload a new file ${item.id}!`);
-    console.log(item);
-  });
+    const uploadChunks = useCallback(async (chunks) => {
+        const promises = chunks.map((chunk, index) => {
+            // problem for later: order of chunks is not guaranteed, but is needed for reassembly
+            // how do I keep track of this?
+            const formData = new FormData();
+            formData.append("chunk_hash", chunk.hash);
 
-  useItemProgressListener((item) => {
-    console.log(`Progress for ${item.id}: ${item.loaded} / ${item.total}`);
-  });
+            // convert the blob to a file (the commented line below could be used if the server accepted blobs)
+            // formData.append("upload", chunk.payload);
+            const file = new File([chunk.payload], `chunk-${index}`, { type: "application/octet-stream" });
+            formData.append("upload", file);
 
-  useItemErrorListener((item) => {
-    console.log(`Error uploading file ${item.id}: ${item.uploadResponse}`);
-  });
+            console.log(formData);
 
-  useItemFinishListener((item) => {
-    console.log(`Finished uploading file ${item.id}!`);
-  });
+            return fetch(UPLOAD_ENDPOINT, {
+                method: "POST",
+                body: formData,
+            });
+        });
 
-  const pickDocument = useCallback(async () => {
-    const result = await DocumentPicker.getDocumentAsync({
-      type: "*/*",
-      copyToCacheDirectory: true,
-      multiple: false,
-    });
+        try {
+            const responses = await Promise.all(promises);
+            const data = await Promise.all(responses.map((response) => response.json()));
+            console.log(data);
+        } catch (err) {
+            console.error(`Chunk upload error: ${err}`);
+        }
+    }, []);
 
-    if (result.canceled === true || result.assets.length === 0) {
-      return;
-    }
+    const getChunkFromBlob = useCallback(async (blob) => {
+        return new Promise((resolve) => {
+            const reader = new FileReader();
 
-    const asset = result.assets[0];
+            reader.onloadend = async () => {
+                const arrayBuffer = reader.result;
+                const uint8Array = new Uint8Array(arrayBuffer);
+   
+                const hash = sha256(uint8Array);
+                const payload = new Blob([uint8Array]);
 
-    try {
-      const file = await fetch(asset.uri);
-      const blob = await file.blob();
-      const fixedBlob = new Blob([blob], { type: "application/octet-stream" });
+                resolve({
+                    payload: payload,
+                    hash: hash,
+                });
+            }
 
-      uploadyContext.upload(fixedBlob, {
-        sendWithFormData: true,
-        params: {
-          fileName: asset.name,
-        },
-      });
-    } catch (err) {
-      console.log(`upload error: ${err}`);
-    }
-  }, [uploadyContext]);
+            reader.readAsArrayBuffer(blob);
+        });
+    }, []);
 
-  return (
-    <View>
-      <Button title="Upload File" onPress={pickDocument} />
-    </View>
-  );
+    const prepareAndUploadBlob = useCallback(async (blob) => {
+        const chunkSize = 2 * 1024 * 1024;
+        const chunks = [];
+
+        let offset = 0;
+        while (offset < blob.size) {
+            const payload = blob.slice(offset, offset + chunkSize);
+            const chunk = await getChunkFromBlob(payload);
+            chunks.push(chunk);
+
+            offset += chunkSize;
+        }
+
+        console.log(`Uploading ${chunks.length} chunks!`);
+
+        await uploadChunks(chunks);
+    }, []);
+
+    const uploadAsset = useCallback(async (asset) => {
+        try {
+            const file = await fetch(asset.uri);
+            const blob = await file.blob();
+            await prepareAndUploadBlob(blob);
+        } catch (err) {
+            console.error(`Upload error: ${err}`);
+        }
+    }, []);
+
+    const startFileUpload = useCallback(async () => {
+        const result = await DocumentPicker.getDocumentAsync({
+            type: "*/*",
+            copyToCacheDirectory: true,
+            multiple: false,
+        });
+
+        if (result.canceled === true || result.assets.length === 0) {
+            return;
+        }
+
+        const asset = result.assets[0];
+        await uploadAsset(asset);
+    }, []);
+
+    return (
+        <View>
+            <Button title="Upload File" onPress={startFileUpload} />
+        </View>
+    );
 };
 
 export default Upload;
